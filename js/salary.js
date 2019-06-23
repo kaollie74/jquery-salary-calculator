fdae1111fde538c7df74944520594a66940c9c71
Kyle Olson <kaollie74@gmail.com>
2019-06-23 17:34:25 -0500

    add additional comments to js file and reformated code

--- js/salary.js
+++ js/salary.js
@@ -6,7 +6,7 @@ let salaryInventory = [];
 function addItem() {
     console.log('in additem');
 
-    // capture user input
+    // capture user input us jQuery and the val method
     let newItem = {
         firstName: $('#firstNameIn').val(),
         lastName: $('#lastNameIn').val(),
@@ -21,7 +21,7 @@ function addItem() {
         console.log(newItem);
         //push newItem object into salaryInventory array.
         salaryInventory.push(newItem);
-        // targets class and empties all the inputs
+        // targets class and empties all the inputs with a specific class
         $('.removeAll').val('');
         // invoke function
         displaySalary();
@@ -37,10 +37,15 @@ function textEmpty() {
 
 
 
-
+// for loop to capture the value of each object key
+// append it to the DOM while.
+// take the annual salaries and divdes by 12 to get Monthly
+// appends the overall monthly salary to the DOM
+// if condition to check if the monthly exceeds 20000
+// set the background to red when it exceeds 200000
 function displaySalary() {
-    let annualSalary = 0;
-    let monthlySalary = 0;
+    let totalAnnualSalary = 0;
+    let totalMonthlySalary = 0;
     console.log('in Display inventory');
     //target id element and set it to el.
     let el = $('#employeeInfoOut');
@@ -58,36 +63,39 @@ function displaySalary() {
         </tr>`)
         // while looping, capture the value of salaryInventory[i].salary
         // and add it to the AnnualSalary variable
-        annualSalary += Number(salaryInventory[i].salary);
+        totalAnnualSalary += Number(salaryInventory[i].salary);
 
     } // end for loop
 
     // take the current annualSalary/12, round up or
     // down depending on decimal and assign to monthlySalary
-    monthlySalary = Math.round(annualSalary / 12);
+    totalMonthlySalary = Math.round(totalAnnualSalary / 12);
 
     // target ID (where i want it to go) and send the current
     // monthlySalary value to the DOM.
-    $('#background').html(monthlySalary);
+    $('#background').html(totalMonthlySalary);
 
     // condition checks if the monthlySalary is over 20,000
-    if (monthlySalary > 20000) {
+    if (totalMonthlySalary > 20000) {
         // targets ID (where I want it to go) and changes the
         // background to red.
         $('#background').parent().addClass('backgroundRed');
-    } else if(monthlySalary < 20000) {
-         $('#background').parent().removeClass('backgroundRed');
+    } else if (totalMonthlySalary < 20000) {
+        // if totalMonthlySalary drops below 20000, remove css class
+        $('#background').parent().removeClass('backgroundRed');
     }
 
 
 } // end displaySalary
 
 function deleteEmployee() {
-
+    // captures the index of element that is removed and sets to index
     let index = salaryInventory.indexOf('tr');
+    //when clicked, removes the closest <tr>
     $(this).closest('tr').remove(); // .closest() removes the first <tr> that it runs in to
+    //removes the specific index from the array.
     salaryInventory.splice(index, 1);
-
+    //active function to recalculate math and append to the DOM
     displaySalary();
 
 } // end deleteEmployee
